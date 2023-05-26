704bc512dcd1523dfe253cc28e4b8c167b3abf61
hn-h <82378612+hn-h@users.noreply.github.com>
2023-05-27 02:22:45 +0300

    Update settings.py

--- movieRecommendationProject/settings.py
+++ movieRecommendationProject/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-o7(=i%_x-yt$kfz$qm%!@#c61p6&+6a-zbeests$sd_2!(0#i3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['moviehn.herokuapp.com']
+# vercel_app/settings.py
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
