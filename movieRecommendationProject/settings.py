2c50dcf6dce6db0b8381c775d6a5dd5d2739ee19
hn-h <82378612+hn-h@users.noreply.github.com>
2023-05-27 02:56:43 +0300

    Update settings.py

--- movieRecommendationProject/settings.py
+++ movieRecommendationProject/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,6 +133,3 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
